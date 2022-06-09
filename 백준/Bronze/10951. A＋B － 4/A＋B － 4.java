0fe0c9b8733b375894dc5a457125c3d72d2fa459
YuJeong-Yun <97975367+YuJeong-Yun@users.noreply.github.com>
2022-06-09 17:26:47 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 13092 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n1, n2;
+
+		while (sc.hasNext()) {
+			n1 = sc.nextInt();
+			n2 = sc.nextInt();
+
+			System.out.println(n1 + n2);
+		}
+	}
+}
