6ddac1888826bdc79d3cc629dd77816678cf46aa
YuJeong-Yun <97975367+YuJeong-Yun@users.noreply.github.com>
2022-06-15 10:54:29 +0900

    [Silver V] Title: 셀프 넘버, Time: 172 ms, Memory: 16128 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+
+public class Main {
+	static int[] nums = new int[10001];
+
+	public static void main(String[] args) throws IOException {
+
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 1; i < 10000; i++) {
+			d(i);
+		} // for
+
+		for (int i = 1; i <= 10000; i++) {
+			if (nums[i] == 0) {
+				sb.append(i + "\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+	public static void d(int num) { // 셀프 넘버 구하는 함수
+		String numS = Integer.toString(num);
+		int newNum = num;
+
+		for (int i = 0; i < numS.length(); i++) {
+			int digit = numS.charAt(i) - '0';
+			newNum += digit;
+		}
+
+		if (newNum <= 10000) {
+			nums[newNum] = 1;
+		}
+	}
+}
