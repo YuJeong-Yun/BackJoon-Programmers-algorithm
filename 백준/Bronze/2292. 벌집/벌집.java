63fb398b09ecca6cd6a55ef968e9da51d48185fa
YuJeong <97975367+YuJeong-Yun@users.noreply.github.com>
2022-06-16 10:43:42 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int num = (N + 4) / 6;
+		int sum = 0;
+		int i = 1, cnt = 1;
+
+		while (sum < num) {
+			sum += i++;
+			cnt++;
+		}
+
+		bw.write(cnt + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
