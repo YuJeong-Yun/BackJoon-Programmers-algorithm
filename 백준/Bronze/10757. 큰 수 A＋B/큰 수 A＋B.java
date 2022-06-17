1939a68c8a208768b3007c938d0973f12aea8091
YuJeong <97975367+YuJeong-Yun@users.noreply.github.com>
2022-06-17 23:18:11 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 240 ms, Memory: 65948 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,11 +11,57 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		String A = st.nextToken();
+		String B = st.nextToken();
 
-		BigInteger A = new BigInteger(st.nextToken());
-		BigInteger B = new BigInteger(st.nextToken());
-		
-		bw.write(A.add(B)+"");
+		String small = "", big = "";
+
+		if (A.length() > B.length()) {
+			big = A;
+			small = B;
+		} else {
+			big = B;
+			small = A;
+		}
+
+		int sum = 0;
+		int over = 0;
+		String result = "";
+		for (int i = 1; i <= small.length(); i++) {
+			sum = big.charAt(big.length() - i) + small.charAt(small.length() - i) - '0' * 2;
+			if (over == 1) {
+				sum++;
+				over = 0;
+			}
+			if (sum >= 10) {
+				over = 1;
+			}
+			result = sum % 10 + result;
+
+		}
+
+		if (A.length() != B.length()) {
+			for (int i = 0; i < big.length() - small.length(); i++) {
+				sum = big.charAt(big.length() - small.length() - 1 - i) - '0';
+				if (over == 1) {
+					sum++;
+					over = 0;
+				}
+				if (sum >= 10) {
+					over = 1;
+				}
+				result = sum % 10 + result;
+			}
+			if (over == 1) {
+				result = 1 + result;
+			}
+		} else {
+			if (over == 1) {
+				result = 1 + result;
+			}
+		}
+
+		bw.write(result);
 		bw.flush();
 		bw.close();
 		br.close();
