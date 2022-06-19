da0b1f9c1e241014d0ce2357ef2139669a11ba65
YUJEONG YUN <yunyj99@naver.com>
2022-06-19 17:58:06 +0900

    [Silver II] Title: 베르트랑 공준, Time: 256 ms, Memory: 27704 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,39 +1,46 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-
-		do {
-			int cnt = 0;
-			for (int i = N + 1; i <= 2 * N; i++) {
-				boolean isPrime = true;
-
-				if (i == 1) {
-					continue;
-				}
-				for (int j = 2; j <= Math.sqrt(i); j++) {
-					if (i % j == 0) {
-						isPrime = false;
-						break;
-					}
-				}
-				if (isPrime == true) {
-					cnt++;
-				}
+		while (true) {
+			int N = Integer.parseInt(br.readLine());
+			if (N == 0) {
+				break;
 			}
-			sb.append(cnt + "\n");
-
-			N = Integer.parseInt(br.readLine());
-		} while (N != 0);
+			if (N == 1) {
+				sb.append("1\n");
+			} else {
+				sb.append(calcPrime(N) + "\n");
+			}
+		}
 
 		System.out.println(sb);
 		br.close();
+	}
+
+	public static int calcPrime(int N) {
+		int cnt = 0;
+		boolean[] isPrime = new boolean[2 * N + 1];
+		Arrays.fill(isPrime, true);
+
+		for (int i = 2; i <= Math.sqrt(2 * N); i++) {
+			if (isPrime[i] == true) {
+				for (int j = i * i; j < isPrime.length; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+
+		for (int i = N + 1; i <= 2 * N; i++) {
+			if (isPrime[i] == true)
+				cnt++;
+		}
+		return cnt;
 	}
 }
