462505c6ec6ae5180a36c4e83a643ab260aa70a6
YUJEONG YUN <yunyj99@naver.com>
2022-06-19 19:14:35 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 356 ms, Memory: 21100 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
+	public static boolean[] noPrime = new boolean[10001];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -12,22 +14,20 @@ public class Main {
 		for (int t = 0; t < T; t++) {
 			int n = Integer.parseInt(br.readLine());
 
-			boolean[] isPrime = new boolean[n + 1];
-			Arrays.fill(isPrime, true);
 
 			for (int i = 2; i <= Math.sqrt(n); i++) {
-				if (isPrime[i] == true) {
+				if (!noPrime[i]) {
 					for (int j = i * i; j <= n; j += i) {
-						isPrime[j] = false;
+						noPrime[j] = true;
 					}
 				}
 			}
 
-			if (isPrime[n / 2]) {
+			if (!noPrime[n / 2]) {
 				sb.append(n / 2 + " " + n / 2 + "\n");
 			} else {
 				for (int i = n / 2 - 1, j = n / 2 + 1; i > 1; i--, j++) {
-					if (isPrime[i] && isPrime[j]) {
+					if (!noPrime[i] && !noPrime[j]) {
 						sb.append(i + " " + j + "\n");
 						break;
 					}
