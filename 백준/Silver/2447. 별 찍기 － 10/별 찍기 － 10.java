6b48d41091a88bae57d3dfe6637cfc9369798044
YUJEONG YUN <yunyj99@naver.com>
2022-06-21 11:47:07 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 480 ms, Memory: 58100 KB -BaekjoonHub

---
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		String[][] stars = new String[n][n];
+
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(stars[i], "*");
+		}
+
+		drawStar(n, stars, n);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				sb.append(stars[i][j]);
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+
+	public static void drawStar(int n, String[][] stars, int seq) {
+		for (int z = 0; z < n / seq; z++) {
+			for (int k = 0; k < n / seq; k++) {
+				drawStarInner(z, k, stars, seq);
+			}
+		}
+
+		if (seq > 3) {
+			drawStar(n, stars, seq / 3);
+		}
+	}
+
+	public static void drawStarInner(int z, int k, String[][] stars, int seq) {
+		for (int j = seq / 3 + z * seq; j < seq / 3 + z * seq + seq / 3; j++) {
+			for (int i = seq / 3 + k * seq; i < seq / 3 + k * seq + seq / 3; i++) {
+				stars[j][i] = " ";
+			}
+		}
+	}
+
+}
