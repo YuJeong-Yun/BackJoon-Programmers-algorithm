846afbab98830b26995682674bedde1f63c8c5f7
YUJEONG YUN <yunyj99@naver.com>
2022-06-21 14:34:48 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 344 ms, Memory: 47984 KB -BaekjoonHub

--- 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,48 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static char[][] arr;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		arr = new char[N][N];
+
+		drawstar(0, 0, N, false);
+
 		StringBuilder sb = new StringBuilder();
-
-		int n = Integer.parseInt(br.readLine());
-		char[][] stars = new char[n][n];
-
-		for (int i = 0; i < n; i++) {
-			Arrays.fill(stars[i], '*');
-		}
-
-		drawStar(n, stars, n);
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				sb.append(stars[i][j]);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(arr[i][j]);
 			}
-			sb.append("\n");
+			sb.append('\n');
 		}
-
-		System.out.println(sb);
-		br.close();
+		System.out.print(sb);
 	}
 
-	public static void drawStar(int n, char[][] stars, int seq) {
-		for (int z = 0; z < n / seq; z++) {
-			for (int k = 0; k < n / seq; k++) {
-				for (int j = seq / 3 + z * seq; j < seq / 3 + z * seq + seq / 3; j++) {
-					for (int i = seq / 3 + k * seq; i < seq / 3 + k * seq + seq / 3; i++) {
-						stars[j][i] = ' ';
-					}
+	public static void drawstar(int x, int y, int N, boolean blank) {
+
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) {
+					drawstar(i, j, size, true);
+				} else {
+					drawstar(i, j, size, false);
 				}
 			}
 		}
-
-		if (seq > 3) {
-			drawStar(n, stars, seq / 3);
-		}
 	}
-
-
 }
