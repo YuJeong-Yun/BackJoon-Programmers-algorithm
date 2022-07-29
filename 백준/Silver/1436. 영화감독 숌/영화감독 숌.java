a30cc4875369f55ba5b8f33e5ed2263fd77048b5
YUJEONG YUN <yunyj99@naver.com>
2022-07-29 21:57:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 176 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -6,30 +6,45 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 
+	public static boolean check(int num) {
+		int chk = 0;
+
+		while (num > 1 && chk != 3) {
+			if (num % 10 == 6) {
+				chk++;
+			} else {
+				chk = 0;
+			}
+			num /= 10;
+		}
+
+		if (chk == 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
 
-		int start = 666;
 		int seq = 0;
-		while (true) {
-			String startS = String.valueOf(start);
-			if (startS.contains("666")) {
+		int num = 666;
+
+		while (seq != N) {
+			if (check(num)) {
 				seq++;
 			}
-			if (seq == N) {
-				break;
-			}
-
-			start++;
+			num++;
 		}
 
-		bw.write(start + "");
+		bw.write(num - 1 + "");
+
 		bw.flush();
 		bw.close();
 		br.close();
-
 	}
 }
