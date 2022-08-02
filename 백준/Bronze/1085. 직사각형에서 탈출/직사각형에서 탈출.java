830afe06083eabe33659dc522132c1059cbe2be6
YUJEONG YUN <yunyj99@naver.com>
2022-08-02 23:09:59 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 132 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+
+		int min = Math.min(w - x, x);
+		min = Math.min(min, h - y);
+		min = Math.min(min, y);
+
+		bw.write(min + "");
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
