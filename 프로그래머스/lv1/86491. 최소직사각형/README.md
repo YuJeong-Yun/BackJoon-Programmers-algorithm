ee65a921fa167fac80773f6b6ba42acac9c88905
YUJEONG YUN <yunyj99@naver.com>
2022-08-24 16:47:43 +0900

    [level 1] Title: 최소직사각형, Time: 1.72 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
 
 ### 성능 요약
 
-메모리: 90.4 MB, 시간: 4.20 ms
+메모리: 92.3 MB, 시간: 1.72 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        
-        int[] maxArr = new int[sizes.length];
-        int[] minArr = new int[sizes.length];
-        
-        int max, min;
-        for(int i = 0; i <  sizes.length; i++) {
-            max = Math.max(sizes[i][0], sizes[i][1]);
-            min = Math.min(sizes[i][0], sizes[i][1]);
             
-            maxArr[i] = max;
-            minArr[i] = min;
+        int max = 0, min = 0;
+        for(int i = 0; i <  sizes.length; i++) {
+            int tempMax = Math.max(sizes[i][0], sizes[i][1]);
+            int tempMin = Math.min(sizes[i][0], sizes[i][1]);
+            
+            max = Math.max(max, tempMax);
+            min = Math.max(min, tempMin);
         }
         
-        answer = Arrays.stream(maxArr).max().getAsInt() * Arrays.stream(minArr).max().getAsInt();
+        answer = max*min;
         
         
         return answer;
