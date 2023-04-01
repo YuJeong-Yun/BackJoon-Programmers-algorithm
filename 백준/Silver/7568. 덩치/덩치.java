df9ec7bf71c36a9a0c06d0dcb0a9dcfa1cc3c738
YUJEONG YUN <yunyj99@naver.com>
2023-04-02 02:06:46 +0900

    [Silver V] Title: 덩치, Time: 140 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,40 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] person = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            person[i][0] = Integer.parseInt(st.nextToken());
+            person[i][1] = Integer.parseInt(st.nextToken());
+        }
 
-		int N = Integer.parseInt(br.readLine());
-		int[][] person = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if (person[i][0] < person[j][0] && person[i][1] < person[j][1]) {
+                    rank++;
+                }
+            }
+            bw.write(rank + " ");
+        }
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-			person[i][0] = Integer.parseInt(st.nextToken());
-			person[i][1] = Integer.parseInt(st.nextToken());
-		}
-
-		for (int i = 0; i < N; i++) {
-			int rank = 1;
-			for (int j = 0; j < N; j++) {
-				if (i == j)
-					continue;
-				if ((person[i][0] < person[j][0]) && (person[i][1] < person[j][1])) {
-					rank++;
-				} 
-			}
-			bw.write(rank + " ");
-		}
-
-		bw.flush();
-		bw.close();
-		br.close();
-	}
 }
