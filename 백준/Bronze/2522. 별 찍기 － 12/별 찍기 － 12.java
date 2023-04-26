290e37e68370a3796bbbcbc2e2de7a2e5abd5c38
YUJEONG YUN <yunyj99@naver.com>
2023-04-27 02:54:45 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 140 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < N - i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
