b5a89fa9183764200d4decc9a5cee2287ed40ba2
YUJEONG YUN <yunyj99@naver.com>
2023-05-01 04:41:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 404 ms, Memory: 23460 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -11,29 +11,26 @@ public class Main {
         String bomb = br.readLine();
         int len = bomb.length();
 
-        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         out:
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            stack.push(c);
+            sb.append(c);
 
-            if (c == bomb.charAt(len - 1) && stack.size() >= len) { // 폭발 문자열의 마지막 글자와 같으면 && 스택에 bomb 길이만큼 남아있으면
+            if (c == bomb.charAt(len - 1) && sb.length() >= len) { // 폭발 문자열의 마지막 글자와 같으면 && 스택에 bomb 길이만큼 남아있으면
                 for (int j = 0; j < len; j++) {
-                    if (stack.get(stack.size() - len + j) != bomb.charAt(j)) { // 폭발 문자열과 다르면 넘어감
+                    if (sb.charAt(sb.length() - len + j) != bomb.charAt(j)) { // 폭발 문자열과 다르면 넘어감
                         continue out;
                     }
                 }
-                for (int j = 0; j < len; j++) {
-                    stack.pop();
-                }
+
+                sb.delete(sb.length() - len, sb.length());
             }
         }
 
 
-        if (!stack.isEmpty()) {
-            for (char c : stack) {
-                bw.write(c + "");
-            }
+        if (sb.length() > 0) {
+            bw.write(sb.toString());
         } else {
             bw.write("FRULA");
         }
