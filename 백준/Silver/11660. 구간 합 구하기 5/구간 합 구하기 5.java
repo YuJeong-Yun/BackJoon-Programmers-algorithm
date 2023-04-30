688347ea2f353acdbc18ae3b95a7f3520d6ccec5
YUJEONG YUN <yunyj99@naver.com>
2023-05-01 00:21:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 928 ms, Memory: 124748 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,30 +9,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            int pre = 0;
+        int[][] board = new int[N + 1][N + 1], dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken()) + pre; // 현재 위치 값 = 현재 위치까지 같은 행 누적 합 값
-                pre = board[i][j];
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + board[i][j];
             }
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken()) - 1, y1 = Integer.parseInt(st.nextToken()) - 1,
-                    x2 = Integer.parseInt(st.nextToken()) - 1, y2 = Integer.parseInt(st.nextToken()) - 1,
-                    sum = 0;
-
-            for (int j = x1; j <= x2; j++) {
-                sum += board[j][y2];
-                if (y1 > 0) {
-                    sum -= board[j][y1 - 1];
-                }
-            }
-            bw.write(sum + "\n");
-
+            int x1 = Integer.parseInt(st.nextToken()), y1 = Integer.parseInt(st.nextToken()),
+                    x2 = Integer.parseInt(st.nextToken()), y2 = Integer.parseInt(st.nextToken());
+            int answer = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+            bw.write(answer + "\n");
         }
 
         bw.flush();
