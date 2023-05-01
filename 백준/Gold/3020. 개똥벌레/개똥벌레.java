9a1b72986dcb23a4259401c861928e3ab7b74f9a
YUJEONG YUN <yunyj99@naver.com>
2023-05-02 00:01:58 +0900

    [Gold V] Title: 개똥벌레, Time: 336 ms, Memory: 35148 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -9,7 +9,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), H = Integer.parseInt(st.nextToken());
 
-        int[] barrier1 = new int[H + 1], barrier2 = new int[H + 1];
+        int[] barrier1 = new int[H + 1], barrier2 = new int[H + 1]; // 각각 석순, 종유석 높이별 개수 담을 배열
         for (int i = 0; i < N; i++) {
             int height = Integer.parseInt(br.readLine());
             if (i % 2 == 0) {
@@ -22,14 +22,13 @@ public class Main {
         int[] destroy1 = new int[H + 1], destroy2 = new int[H + 1];
         for (int i = H - 1; i >= 1; i--) {
             destroy1[i] = barrier1[i] + destroy1[i + 1];
-        }
-        for (int i = 1; i <= H; i++) {
-            destroy2[i] = barrier2[H - i + 1] + destroy2[i - 1];
+            destroy2[i] = barrier2[i] + destroy2[i + 1];
+
         }
 
-        int min = Integer.MAX_VALUE, cnt = 0;
+        int min = Integer.MAX_VALUE, cnt = 0; // 파괴해야하는 장애물 최솟값, 구간의 수
         for (int i = 1; i <= H; i++) {
-            int destroy = destroy1[i] + destroy2[i];
+            int destroy = destroy1[i] + destroy2[H - i + 1];
             if (destroy < min) {
                 min = destroy;
                 cnt = 1;
