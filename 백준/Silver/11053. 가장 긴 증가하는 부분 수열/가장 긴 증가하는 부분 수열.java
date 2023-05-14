a15fd462eb6ea60967e2666fc4eb6bc4be212c12
YUJEONG YUN <yunyj99@naver.com>
2023-05-14 17:18:15 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dy = new int[N + 1];
+
+        dy[1] = 1;
+        int answer = 1;
+        for (int i = 2; i <= N; i++) {
+            int max = 0;
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j] && dy[j] > max) {
+                    max = dy[j];
+                }
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
