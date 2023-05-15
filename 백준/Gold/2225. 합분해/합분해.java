ddd9426c5e8d5bfdeda906989aa62e8c5e396e93
YUJEONG YUN <yunyj99@naver.com>
2023-05-16 02:07:09 +0900

    [Gold V] Title: 합분해, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[K + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+            }
+        }
+
+        bw.write(dp[K][N] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
