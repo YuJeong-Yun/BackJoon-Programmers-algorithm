61f895e957e9f908899925486084853f0be17d91
YUJEONG YUN <yunyj99@naver.com>
2023-07-30 01:39:09 +0900

    [Silver II] Title: 친구, Time: 140 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -2,43 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static List<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(); // 직접 친구 관계 담을 리스트
+    static int answer = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        List<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(); // 직접 친구 관계 담을 리스트
-        List<HashSet<Integer>> friend = new ArrayList<HashSet<Integer>>(); //  2-친구 관계 담을 리스트
+        N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            list.add(new ArrayList<>());
-            friend.add(new HashSet<>());
+            graph.add(new ArrayList<>());
         }
         // 직접 친구 관계 저장
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < N; j++) {
                 if (s.charAt(j) == 'Y') {
-                    list.get(i).add(j);
+                    graph.get(i).add(j);
                 }
             }
         }
 
-        // 2-친구 관계 찾기
         for (int i = 0; i < N; i++) {
-            for (int j : list.get(i)) { // i의 직접 친구
-                friend.get(i).add(j);
-                for (int k : list.get(j)) { // i의 2-친구
-                    friend.get(i).add(k);
-                }
-            }
+            BFS(i);
         }
-
-
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            answer = Math.max(answer, friend.get(i).size());
-        }
-        if (answer > 0) answer -= 1; // 자기 자신은 제외이므로 -1
 
         bw.write(answer + "");
         bw.flush();
@@ -46,4 +34,29 @@ public class Main {
         br.close();
 
     }
+
+    public static void BFS(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        boolean[] ch = new boolean[N];
+        int tmp = 0; // 2-친구 수
+
+        ch[start] = true;
+        for (int i : graph.get(start)) {
+            que.offer(i);
+            tmp++;
+            ch[i] = true;
+        }
+
+        while (!que.isEmpty()) {
+            int friend = que.poll();
+            for (int friend_2 : graph.get(friend)) {
+                if (!ch[friend_2]) {
+                    ch[friend_2] = true;
+                    tmp++;
+                }
+            }
+        }
+        answer = Math.max(answer, tmp);
+    }
+
 }
