106cab99d5161da3002c65e479643dfb03ab2e3d
YUJEONG YUN <yunyj99@naver.com>
2024-03-14 18:49:20 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 232 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,40 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+
+
+import java.io.*;
 
 public class Main {
-	public static boolean[] noPrime = new boolean[10001];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
+            int n1 = n / 2;
+            int n2 = n / 2;
+            while (n1 > 1) {
+                if (isPrimeNum(n1) && isPrimeNum(n2)) {
+                    bw.write(n1 + " " + n2 + "\n");
+                    break;
+                }
+                n1--;
+                n2++;
+            }
+        }
 
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-			for (int i = 2; i <= Math.sqrt(n); i++) {
-				if (!noPrime[i]) {
-					for (int j = i * i; j <= n; j += i) {
-						noPrime[j] = true;
-					}
-				}
-			}
-
-			if (!noPrime[n / 2]) {
-				sb.append(n / 2 + " " + n / 2 + "\n");
-			} else {
-				for (int i = n / 2 - 1, j = n / 2 + 1; i > 1; i--, j++) {
-					if (!noPrime[i] && !noPrime[j]) {
-						sb.append(i + " " + j + "\n");
-						break;
-					}
-				}
-			}
-		}
-		System.out.println(sb);
-		br.close();
-	}
+    public static boolean isPrimeNum(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
