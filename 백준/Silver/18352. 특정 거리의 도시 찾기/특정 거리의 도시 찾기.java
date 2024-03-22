650e835d36ace468dbc41b8b24ae66825289f306
YUJEONG YUN <yunyj99@naver.com>
2024-03-22 19:13:35 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1088 ms, Memory: 259932 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,80 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int K;
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static boolean[] ch;
+    public static List<Integer> answer = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
+        K = Integer.parseInt(st.nextToken()); 
+        int X = Integer.parseInt(st.nextToken()); 
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        ch = new boolean[N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph.get(s).add(e);
+        }
+
+        BFS(X);
+
+        if (answer.isEmpty()) {
+            bw.write("-1");
+        } else {
+            Collections.sort(answer);
+            for (int n : answer) {
+                bw.write(n + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void BFS(int V) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(V);
+        ch[V] = true;
+
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            if (level == K) {
+                for (int i = 0; i < len; i++) {
+                    int tmp = queue.poll();
+                    for (int next : graph.get(tmp)) {
+                        if (!ch[next]) {
+                            ch[next] = true;
+                            answer.add(next);
+                        }
+                    }
+                }
+            } else {
+                for (int i = 0; i < len; i++) {
+                    int tmp = queue.poll();
+                    for (int next : graph.get(tmp)) {
+                        if (!ch[next]) {
+                            ch[next] = true;
+                            queue.offer(next);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+    }
+}
