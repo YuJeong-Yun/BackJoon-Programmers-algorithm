5d8ff9a13ec10cda997e291d4c749811787c72a2
YUJEONG YUN <yunyj99@naver.com>
2024-04-01 20:00:23 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 972 ms, Memory: 262112 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,27 +1,33 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
     public static int K;
+    public static int N;
     public static List<List<Integer>> graph = new ArrayList<>();
-    public static boolean[] ch;
-    public static List<Integer> answer = new ArrayList<>();
+    public static int[] dist;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken()); // 시작점
+        dist = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
 
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
-        ch = new boolean[N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
@@ -29,43 +35,38 @@ public class Main {
             graph.get(s).add(e);
         }
 
-        BFS(X);
+        DFS(0, X);
+        dist[X] = 0;
 
-        if (answer.isEmpty()) {
-            bw.write("-1");
-        } else {
-            Collections.sort(answer);
-            for (int n : answer) {
-                bw.write(n + "\n");
+
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == K) {
+                flag = true;
+                sb.append(i + "\n");
             }
+        }
+        if (flag) {
+            System.out.println(sb);
+        } else {
+            bw.write("-1");
         }
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void BFS(int V) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(V);
-        ch[V] = true;
+    public static void DFS(int L, int node) {
+        if (L > K) {
+            return;
+        }
 
-        int level = 1;
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            for (int i = 0; i < len; i++) {
-                int tmp = queue.poll();
-                for (int next : graph.get(tmp)) {
-                    if (!ch[next]) {
-                        ch[next] = true;
-                        if (level == K) {
-                            answer.add(next);
-                        } else {
-                            queue.offer(next);
-                        }
-                    }
-                }
+        for (int tmp : graph.get(node)) {
+            if (dist[tmp] > L + 1) {
+                dist[tmp] = L + 1;
+                DFS(L + 1, tmp);
             }
-            level++;
         }
     }
 }
