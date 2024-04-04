482a7bdf0382a489a21bf421668baded451361c9
YUJEONG YUN <yunyj99@naver.com>
2024-04-04 20:54:37 +0900

    [Gold I] Title: 열쇠, Time: 352 ms, Memory: 34364 KB -BaekjoonHub

---
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+public class Main {
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    public static int h, w, answer;
+    public static boolean[] key;
+    public static char[][] board;
+    public static List<Point> entranceList;
+    public static boolean check = true;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            answer = 0;
+            key = new boolean[26];
+            entranceList = new ArrayList<>();
+
+            board = new char[h][w];
+            for (int j = 0; j < h; j++) {
+                String s = br.readLine();
+                for (int k = 0; k < w; k++) {
+                    char c = s.charAt(k);
+                    board[j][k] = c;
+                }
+            }
+            String s = br.readLine();
+            if (!s.equals("0")) {
+                for (int j = 0; j < s.length(); j++) {
+                    key[s.charAt(j) - 'a'] = true;
+                }
+            }
+
+            for (int j = 0; j < h; j++) {
+                for (int k = 0; k < w; k++) {
+                    if (j == 0 || j == h - 1 || k == 0 || k == w - 1) {
+                        char c = board[j][k];
+                        if (c != '*') {
+                            entranceList.add(new Point(j, k));
+                        }
+                    }
+                }
+            }
+            boolean restart = BFS();
+            while (restart) {
+                restart = BFS();
+            }
+
+            sb.append(answer + "\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static boolean BFS() {
+        Queue<Point> queue = new LinkedList<>();
+
+        boolean[][] ch = new boolean[h][w];
+        for (Point entrance : entranceList) {
+            if (Character.isUpperCase(board[entrance.x][entrance.y]) && !key[board[entrance.x][entrance.y] - 'A']) {
+                continue;
+            }
+            queue.offer(entrance);
+
+            ch[entrance.x][entrance.y] = true;
+            if (board[entrance.x][entrance.y] == '$') {
+                answer++;
+                board[entrance.x][entrance.y] = '.';
+            } else if (Character.isLowerCase(board[entrance.x][entrance.y])) {
+                key[board[entrance.x][entrance.y] - 'a'] = true;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Point tmp = queue.poll();
+
+            for (int i = 0; i < dx.length; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                if (nx >= 0 && nx < h && ny >= 0 && ny < w && board[nx][ny] != '*' && !ch[nx][ny]) {
+                    char nChar = board[nx][ny];
+                    if (Character.isUpperCase(nChar) && !key[nChar - 'A']) {
+                        continue;
+                    }
+                    if (nChar == '$') {
+                        answer++;
+                        board[nx][ny] = '.';
+                    } else if (Character.isLowerCase(nChar) && !key[nChar - 'a']) {
+                        key[nChar - 'a'] = true;
+                        return true;
+                    }
+                    ch[nx][ny] = true;
+                    queue.offer(new Point(nx, ny));
+
+                }
+            }
+        }
+        return false;
+    }
+
+}
